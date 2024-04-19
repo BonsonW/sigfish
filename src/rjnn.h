0acf9fb69e07a61210e3b6129d4c6b012b85dd55
BonsonW <bonson.ym@gmail.com>
2024-04-19 13:40:02 +1000

    modify adaptor params

--- src/rjnn.h
+++ src/rjnn.h
@@ -34,11 +34,11 @@ typedef struct {
     .std_scale = 0.3, \
     .corrector = 800, \
     .seg_dist = 1200, \
-    .window = 300, \
+    .window = 100, \
     .error = 5, \
     .min_seg_len = 1200, \
-    .chunk_size = 1600, \
-    .start_chunks = 1, \
+    .chunk_size = 600, \
+    .start_chunks = 3, \
 } \
 
 typedef struct jnnv3_astate_s {
