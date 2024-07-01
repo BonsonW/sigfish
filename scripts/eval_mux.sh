0fa29b7aaeeb7265c2eb78aec344002bf8a3c559
Bonson Wong <boson.ym@gmail.com>
2024-07-01 10:59:59 +1000

    udpate scripts and gitignore

--- scripts/eval_mux.sh
+++ scripts/eval_mux.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+# run on: pore_scan_data_XXXXXXX.csv
+
 scan_data=$1
+split=0 # for splitting data on odd and even channels
+
+# temp data
 freq=freq.csv
 entries=entries.csv
-split=true # for splitting channels into odds and evens
 
 die() {
     echo "$@" 1>&2
     exit 1
 }
 
-# awk -F, '{ print $29", "$27", "$37 }' $scan_data | awk '!x[$0]++' > out.csv
-
 # cut columns: channel=1, mux_scan_asessment=27, seconds_since_start_of_run=37
-# sort
-awk -F, '{ print $37" "$27" "$1 }' $scan_data | tail -n +2 | sort -nk1,2 > $entries
+{ awk -F, '{ print $37" "$27" "$1 }' $scan_data | tail -n +2 | sort -nk1,2 > $entries; } || die "failed to collect mux data entries"
 
-if $split
+if [ $split -eq 1 ]
 then
-    grep "^.*[02468]$" $entries | awk '{ print $1" "$2 }' | uniq -c | awk '{ print $2","$3","$1 }' > "even_${freq}"
-    grep "^.*[13579]$" $entries | awk '{ print $1" "$2 }' | uniq -c | awk '{ print $2","$3","$1 }' > "odd_${freq}"
+    { grep "^.*[02468]$" $entries | awk '{ print $1" "$2 }' | uniq -c | awk '{ print $2","$3","$1 }' > "even_${freq}"; } || die "failed to collect frequnecy data"
+    { grep "^.*[13579]$" $entries | awk '{ print $1" "$2 }' | uniq -c | awk '{ print $2","$3","$1 }' > "odd_${freq}"; } || die "failed to collect frequnecy data"
 else
-    awk '{ print $1" "$2 }' $entries | uniq -c | awk '{ print $2","$3","$1 }' > $freq
+    { awk '{ print $1" "$2 }' $entries | uniq -c | awk '{ print $2","$3","$1 }' > $freq; } || die "failed to collect frequnecy data"
 fi
 
-export split=$split
+export split=${split}
 
 python3 -c "
 
@@ -82,11 +83,24 @@ def run(freq_path, out_path):
                 + '\n'
             )
 
-split = os.getenv('split')
+split = int(os.getenv('split'))
 
-if split:
+if split == 1:
     run('odd_freq.csv', 'odd_freq_table.csv')
     run('even_freq.csv', 'even_freq_table.csv')
 else:
     run('freq.csv', 'freq_table.csv')
 "
+
+# cleanup
+rm $entries || die "could not remove entries file"
+if [ $split -eq 1 ]
+then
+    rm "even_$freq" || die "could not remove frequency file"
+    rm "odd_$freq" || die "could not remove frequency file"
+else
+    rm "$freq" || die "could not remove frequency file"
+fi 
+
+
+echo "all done!"
